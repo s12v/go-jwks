91c9b8ea74380ba8bbca74afaea18127f1c26ec8
imle <stevenimle@gmail.com>
2021-03-04 02:49:47 -0500

    change way of working with default logging

--- logger.go
+++ logger.go
@@ -8,7 +8,14 @@ type jwksLogger interface {
 	Printf(format string, v ...interface{})
 }
 
-var logger jwksLogger = log.Default()
+// This is necessary to work around go1.12 requirement
+type defaultLogger struct{}
+
+func (defaultLogger) Printf(format string, v ...interface{}) {
+	log.Printf(format, v...)
+}
+
+var logger jwksLogger = defaultLogger{}
 
 func SetLogger(l jwksLogger) {
 	logger = l
